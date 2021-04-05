06e2e5c84107dd3ab8c8be4f8864090905fb26f3
Daniel Urbanowicz <urbanowicz.daniel@gmail.com>
2021-04-05 12:55:04 +0200

    add ErrorBoundary

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,6 @@ export default class Card extends Component {
 		const { name, email, id } = this.props.user;
 		return (
 			<>
-				{/* <h1>RoboFriends</h1> */}
 				<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
 					<img src={`https://robohash.org/${id}?200x200`} alt='robot' />
 					<div>
